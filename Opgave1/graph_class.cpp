9f90835c1aa2784afb9036540cfce7c0626d6f45
Ehabben10 <202107666@post.au.dk>
2024-04-03 10:46:32 +0200

    opgave1

--- Opgave1/graph_class.cpp
+++ Opgave1/graph_class.cpp
@@ -1,32 +1,39 @@
-using namespace std;
 #include "graph_class.h"
 #include <queue>
 #include <climits>
+#include <iostream>
+using namespace std;
 
-void Graph::addEdge(int u, int v) {
+void Graph::addEdge(int u, int v)
+{
 	adj[u].push_back(v);
 	adj[v].push_back(u);
 }
 
-void Graph::addDirectedEdge(int u, int v) {
+void Graph::addDirectedEdge(int u, int v)
+{
 	adj[u].push_back(v);
 }
 
-void Graph::dfs(int vertex, vector < int >&path) {
-	stack < int >sorting;
-	vector < bool >visited(adj.size());
+void Graph::dfs(int vertex, vector<int> &path)
+{
+	stack<int> sorting;
+	vector<bool> visited(adj.size());
 
-	for (int v = 0; v < adj.size(); ++v) {
+	for (int v = 0; v < adj.size(); ++v)
+	{
 		visited[v] = false;
 	}
 	dfs(vertex, visited, path, sorting);
 }
 
-void Graph::bfs(int s, vector < int >&path, vector < int >&dist) {
-	queue < int >q;
-	vector < bool >visited(adj.size());
+void Graph::bfs(int s, vector<int> &path, vector<int> &dist)
+{
+	queue<int> q;
+	vector<bool> visited(adj.size());
 
-	for (int v = 0; v < adj.size(); ++v) {
+	for (int v = 0; v < adj.size(); ++v)
+	{
 		dist[v] = INT_MAX;
 		visited[v] = false;
 	}
@@ -34,11 +41,14 @@ void Graph::bfs(int s, vector < int >&path, vector < int >&dist) {
 	visited[s] = true;
 	q.push(s);
 
-	while (!q.empty()) {
+	while (!q.empty())
+	{
 		s = q.front();
 		q.pop();
-		for (auto w:adj[s]) {
-			if (!visited[w]) {
+		for (auto w : adj[s])
+		{
+			if (!visited[w])
+			{
 				dist[w] = dist[s] + 1;
 				visited[w] = true;
 				path[w] = s;
@@ -48,17 +58,21 @@ void Graph::bfs(int s, vector < int >&path, vector < int >&dist) {
 	}
 }
 
-int Graph::connectedComponents() {
+int Graph::connectedComponents()
+{
 	int components = 0;
-	stack < int >sorting;
-	vector < int >path(adj.size());
-	vector < bool >visited(adj.size());
-	for (int v = 0; v < adj.size(); ++v) {
+	stack<int> sorting;
+	vector<int> path(adj.size());
+	vector<bool> visited(adj.size());
+	for (int v = 0; v < adj.size(); ++v)
+	{
 		visited[v] = false;
 	}
 
-	for (int v = 0; v < adj.size(); ++v) {
-		if (!visited[v]) {
+	for (int v = 0; v < adj.size(); ++v)
+	{
+		if (!visited[v])
+		{
 			++components;
 			dfs(v, visited, path, sorting);
 		}
@@ -67,22 +81,26 @@ int Graph::connectedComponents() {
 	return components;
 }
 
-void Graph::topologicalSort(int v) {
-	stack < int >sorting;
-	vector < int >path(adj.size());
-	vector < bool >visited(adj.size());
+void Graph::topologicalSort(int v)
+{
+	stack<int> sorting;
+	vector<int> path(adj.size());
+	vector<bool> visited(adj.size());
 
-	for (int v = 0; v < adj.size(); ++v) {
+	for (int v = 0; v < adj.size(); ++v)
+	{
 		visited[v] = false;
 	}
 	dfs(v, visited, path, sorting);
-	while (sorting.empty() == false) {
+	while (sorting.empty() == false)
+	{
 		cout << sorting.top() << " ";
 		sorting.pop();
 	}
 }
 
-void Graph::addWeightedEdge(int u, int v, int w) {
+void Graph::addWeightedEdge(int u, int v, int w)
+{
 	adj[u].push_back(v);
 	weight[u][v] = w;
 }
@@ -119,68 +137,94 @@ void Graph::addWeightedEdge(int u, int v, int w) {
 // 	}
 // }
 
-void Graph::dijkstra(int start, int goal, vector<int>& path, vector<int>& dist) {
-    // Sammenligner knuder som der bruges i prioriteringskøen
-    struct pair_comp {
-        constexpr bool operator()(pair<int, int> const& a, pair<int, int> const& b) const noexcept {
-            return a.first > b.first; // Sammenligner afstand (lavest først)
-        }
-    };
-    // Prioriteringskø til at vælge knuden med den mindste afstand først
-    priority_queue<pair<int, int>, vector<pair<int, int>>, pair_comp> q;
-    vector<bool> visited(adj.size()); // Liste over besøgte knuder
+vector<int> Graph::dijkstra(int start, int goal, vector<int> &path, vector<int> &dist)
+{
+	// Sammenligner knuder som der bruges i prioriteringskøen
+	struct pair_comp
+	{
+		constexpr bool operator()(pair<int, int> const &a, pair<int, int> const &b) const noexcept
+		{
+			return a.first > b.first; // Sammenligner afstand (lavest først)
+		}
+	};
+	// Prioriteringskø til at vælge knuden med den mindste afstand først
+	priority_queue<pair<int, int>, vector<pair<int, int>>, pair_comp> q;
+	vector<bool> visited(adj.size()); // Liste over besøgte knuder
 
-    // Finder afstandene til alle knuder til uendelig og besøgte til falsk
-    for (int v = 0; v < adj.size(); ++v) {
-        dist[v] = INFINITY; // til start er afstanden uendelig
-        visited[v] = false;
-        path[v] = -1; // Ingen knude er endnu besøgt
-    }
-    dist[start] = 0; // Afstand til startknuden er 0
-    q.push(make_pair(dist[start], start)); // Tilføj startknuden til prioriteringskøen
+	// Finder afstandene til alle knuder til uendelig og besøgte til falsk
+	for (int v = 0; v < adj.size(); ++v)
+	{
+		dist[v] = INFINITY; // til start er afstanden uendelig
+		visited[v] = false;
+		path[v] = -1; // Ingen knude er endnu besøgt
+	}
+	dist[start] = 0;					   // Afstand til startknuden er 0
+	q.push(make_pair(dist[start], start)); // Tilføj startknuden til prioriteringskøen
 
-    //  Dijkstras algoritme køres indtil målknuden er nået eller køen er tom
-    while (!q.empty()) {
-        int u = q.top().second; // Få den knude med lavest afstand fra køen
-        q.pop();
-        visited[u] = true; //  knuden er besøgt
-        if (u == goal)
-            break; // Tidlig afslutning hvis målknuden findes
-        // Gennemgår alle naboknuderne til den valgte knude
-        for (auto v : adj[u]) {
-            // Opdater afstanden til naboknuden, hvis den er mindre end den nuværende
-            if (!visited[v] && dist[u] != INFINITY && dist[u] + weight[u][v] < dist[v]) {
-                dist[v] = dist[u] + weight[u][v]; // Opdater afstanden
-                path[v] = u; // Opdater forældreknuden til naboknuden
-                q.push(make_pair(dist[v], v)); // Tilføj naboknuden til prioriteringskøen
-            }
-        }
-    }
+	//  Dijkstras algoritme køres indtil målknuden er nået eller køen er tom
+	while (!q.empty())
+	{
+		int u = q.top().second; // Få den knude med lavest afstand fra køen
+		q.pop();
+		visited[u] = true; //  knuden er besøgt
+		if (u == goal)
+			break; // Tidlig afslutning hvis målknuden findes
+		// Gennemgår alle naboknuderne til den valgte knude
+		for (auto v : adj[u])
+		{
+			// Opdater afstanden til naboknuden, hvis den er mindre end den nuværende
+			if (!visited[v] && dist[u] != INFINITY && dist[u] + weight[u][v] < dist[v])
+			{
+				dist[v] = dist[u] + weight[u][v]; // Opdater afstanden
+				path[v] = u;					  // Opdater forældreknuden til naboknuden
+				q.push(make_pair(dist[v], v));	  // Tilføj naboknuden til prioriteringskøen
+			}
+		}
+	}
+
+	vector<int> result;
+	if (visited[goal] == false)
+	{
+		return result;
+	}
+	int v = goal;
+	while (v != -1)
+	{
+		result.push_back(v);
+		v = path[v];
+	}
+	reverse(result.begin(), result.end());
+
+	return result;
 }
 
+void Graph::dijkstra_nopq(int s, vector<int> &path, vector<int> &dist)
+{
+	priority_queue<int> q;
+	vector<bool> visited(adj.size());
 
-
-void Graph::dijkstra_nopq(int s, vector < int >&path, vector < int >&dist) {
-	priority_queue < int >q;
-	vector < bool >visited(adj.size());
-
-	for (int v = 0; v < adj.size(); ++v) {
+	for (int v = 0; v < adj.size(); ++v)
+	{
 		dist[v] = INFINITY;
 		visited[v] = false;
 	}
 	dist[s] = 0;
-	for (int c = 0; c < adj.size(); c++) {
-		int u, min = INFINITY;	// find minimum node
-		for (int v = 0; v < adj.size(); v++) {
-			if (!visited[v] && dist[v] <= min) {
+	for (int c = 0; c < adj.size(); c++)
+	{
+		int u, min = INFINITY; // find minimum node
+		for (int v = 0; v < adj.size(); v++)
+		{
+			if (!visited[v] && dist[v] <= min)
+			{
 				min = dist[v];
 				u = v;
 			}
 		}
 		visited[u] = true;
-		for (auto v:adj[u]) {
-			if (!visited[v] && weight[u][v] && dist[u] != INT_MAX
-					&& dist[u] + weight[u][v] < dist[v]) {
+		for (auto v : adj[u])
+		{
+			if (!visited[v] && weight[u][v] && dist[u] != INT_MAX && dist[u] + weight[u][v] < dist[v])
+			{
 				dist[v] = dist[u] + weight[u][v];
 				path[v] = u;
 			}
@@ -188,21 +232,28 @@ void Graph::dijkstra_nopq(int s, vector < int >&path, vector < int >&dist) {
 	}
 }
 
-void Graph::allPairs(Matrix < int >&path, Matrix < int >&dist) {
+void Graph::allPairs(Matrix<int> &path, Matrix<int> &dist)
+{
 	int n = adj.size();
 	// Initialize d and path
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
 			dist[i][j] = weight[i][j];
 			path[i][j] = -1;
 		}
 	}
 
-	for (int k = 0; k < n; ++k) {
+	for (int k = 0; k < n; ++k)
+	{
 		// Consider each vertex as an intermediate
-		for (int i = 0; i < n; ++i) {
-			for (int j = 0; j < n; ++j) {
-				if (dist[i][k] + dist[k][j] < dist[i][j]) {
+		for (int i = 0; i < n; ++i)
+		{
+			for (int j = 0; j < n; ++j)
+			{
+				if (dist[i][k] + dist[k][j] < dist[i][j])
+				{
 					// Update shortest path
 					dist[i][j] = dist[i][k] + dist[k][j];
 					path[i][j] = k;
@@ -212,13 +263,16 @@ void Graph::allPairs(Matrix < int >&path, Matrix < int >&dist) {
 	}
 }
 
-int Graph::kruskalMST() {
-	int mst_wt = 0;	// Initialize result
-	vector < pair < int, pair < int, int >>>edges;
+int Graph::kruskalMST()
+{
+	int mst_wt = 0; // Initialize result
+	vector<pair<int, pair<int, int>>> edges;
 
-	for (int i = 0; i < adj.size(); i++) {
-		for (auto j:adj[i]) {
-			edges.push_back( { weight[i][j], { i, j} });
+	for (int i = 0; i < adj.size(); i++)
+	{
+		for (auto j : adj[i])
+		{
+			edges.push_back({weight[i][j], {i, j}});
 		}
 	}
 
@@ -229,8 +283,9 @@ int Graph::kruskalMST() {
 	DisjSets ds(adj.size());
 
 	// Iterate through all sorted edges
-	vector < pair < int, pair < int, int >>>::iterator it;
-	for (it = edges.begin(); it != edges.end(); it++) {
+	vector<pair<int, pair<int, int>>>::iterator it;
+	for (it = edges.begin(); it != edges.end(); it++)
+	{
 		int u = it->second.first;
 		int v = it->second.second;
 
@@ -240,7 +295,8 @@ int Graph::kruskalMST() {
 		// Check if the selected edge is creating
 		// a cycle or not (Cycle is created if u
 		// and v belong to same set)
-		if (set_u != set_v) {
+		if (set_u != set_v)
+		{
 			// Current edge will be in the MST
 			cout << u << " - " << v << ", ";
 
@@ -255,39 +311,47 @@ int Graph::kruskalMST() {
 	return mst_wt;
 }
 
-int Graph::primMST() {
-	int mst_wt = 0;	// Initialize result
-	vector <int>parent(adj.size());	// Array to store MST
-	vector <int>key(adj.size());	// Values to pick minimum weight edge in cut
-	vector <bool>visited(adj.size());	// To represent set of vertices included
+int Graph::primMST()
+{
+	int mst_wt = 0;					  // Initialize result
+	vector<int> parent(adj.size());	  // Array to store MST
+	vector<int> key(adj.size());	  // Values to pick minimum weight edge in cut
+	vector<bool> visited(adj.size()); // To represent set of vertices included
 
 	// Initialize all keys as INFINITE
-	for (int i = 0; i < adj.size(); i++) {
+	for (int i = 0; i < adj.size(); i++)
+	{
 		key[i] = INFINITY, visited[i] = false;
 	}
 
 	// Always include first 1st vertex in MST, make sure it is picked first.
 	key[0] = 0;
-	parent[0] = -1;				// First node is always root of MST
+	parent[0] = -1; // First node is always root of MST
 
 	// The MST will have V vertices
-	for (int count = 0; count < adj.size(); count++) {
+	for (int count = 0; count < adj.size(); count++)
+	{
 		// Pick the minimum key vertex not yet included in MST
 		int min = INFINITY, u;
-		for (int v = 0; v < adj.size(); v++) {
-			if (visited[v] == false && key[v] < min) {
+		for (int v = 0; v < adj.size(); v++)
+		{
+			if (visited[v] == false && key[v] < min)
+			{
 				min = key[v], u = v;
 			}
 		}
 		// Add the picked vertex to the MST Set
 		visited[u] = true;
-		if (u != 0) {
+		if (u != 0)
+		{
 			mst_wt += min;
 			cout << u << " - " << parent[u] << ", ";
 		}
 		// Update key/parent of the adjacent vertices of the picked vertex.
-		for (auto v:adj[u]) {
-			if (weight[u][v] && visited[v] == false && weight[u][v] < key[v]) {
+		for (auto v : adj[u])
+		{
+			if (weight[u][v] && visited[v] == false && weight[u][v] < key[v])
+			{
 				parent[v] = u, key[v] = weight[u][v];
 			}
 		}
@@ -295,11 +359,13 @@ int Graph::primMST() {
 	return mst_wt;
 }
 
-void Graph::print() {
-	for (int v = 0; v < adj.size(); ++v) {
+void Graph::print()
+{
+	for (int v = 0; v < adj.size(); ++v)
+	{
 		cout << "Adjacency list of vertex " << v;
-	for (auto x:adj[v])
-		cout << " -> " << x;
+		for (auto x : adj[v])
+			cout << " -> " << x;
 		cout << endl;
 	}
 }
--- Opgave3/graph_class.h
+++ Opgave3/graph_class.h
@@ -1,3 +1,4 @@
+using namespace std;
 #include <vector>
 #include <queue>
 #include <stack>
@@ -7,23 +8,17 @@
 #include "matrix_class.h"
 #include "disjoint_sets.h"
 
-using namespace std;
-
 #define INFINITY 1000000
 
-class Graph
-{
-private:
+class Graph {
+  private:
 	vector<vector<int>> adj;
 	vector<vector<int>> weight;
 
-	void dfs(int v, vector<bool> &visited, vector<int> &path, stack<int> &sorting)
-	{
+	void dfs(int v, vector<bool>& visited, vector<int>& path, stack<int>& sorting) {
 		visited[v] = true;
-		for (auto w : adj[v])
-		{
-			if (!visited[w])
-			{
+		for (auto w : adj[v]) {
+			if (!visited[w]) {
 				path[w] = v;
 				dfs(w, visited, path, sorting);
 			}
@@ -31,11 +26,9 @@ private:
 		sorting.push(v);
 	}
 
-public:
-	Graph(int vertices = 1) : adj(vertices), weight(vertices)
-	{
-		for (int i = 0; i < vertices; i++)
-		{
+  public:
+	Graph(int vertices = 1) : adj(vertices), weight(vertices) {
+		for (int i = 0; i < vertices; i++) {
 			weight[i].resize(vertices, INFINITY);
 			weight[i][i] = 0; // Initialize self-edges with zero cost.
 		}
@@ -44,8 +37,8 @@ public:
 	void addEdge(int u, int v);
 	void addDirectedEdge(int u, int v);
 	void addWeightedEdge(int u, int v, int w);
-	void dfs(int v, vector<int> &path);
-	void bfs(int s, vector<int> &path, vector<int> &dist);
+	void dfs(int v, vector<int>& path);
+	void bfs(int s, vector<int>& path, vector<int>& dist);
 	void topologicalSort(int v);
 	/**
 	 * Count the number of connected components in the graph.
@@ -57,7 +50,7 @@ public:
 	 * in the graph. Path will store the edges of the shortest paths and dist
 	 * the actual distances.
 	 */
-	vector<int> dijkstra(int start, int goal, vector<int> &path, vector<int> &dist);
+	void dijkstra(int s, vector<int>& path, vector<int>& dist);
 	void dijkstra_nopq(int s, vector<int> &path, vector<int> &dist);
 	/**
 	 * Compute all-shortest paths.
@@ -67,7 +60,7 @@ public:
 	 * dist[i][i] is set to a negative value.
 	 * Actual path can be computed using path[][].
 	 */
-	void allPairs(Matrix<int> &path, Matrix<int> &dist);
+	void allPairs(Matrix<int>& path, Matrix<int>& dist);
 
 	int kruskalMST();
 	int primMST();
